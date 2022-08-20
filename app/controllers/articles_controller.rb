202951be931f05848859293f9a888cbb0ec667c3
Melrose1 <melisamr@hotmail.com>
2022-08-20 00:36:00 -0400

    show categories, add, delete categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,20 @@ class ArticlesController < ApplicationController
     def edit
        # puts "\n\n\n #{@article.id} \n\n\n"
        # puts "\n\n\n #{@article.persisted?} \n\n\n" 
+       @categories = Category.all
     end
     def new
         @article = Article.new
+        @categories = Category.all
     end
     def create
         @article = current_user.articles.create(article_params) 
+        @article.save_categories
         redirect_to @article
     end
     def update
         @article.update(article_params)
+        @article.save_categories
         redirect_to @article
     end
     def destroy
@@ -33,6 +37,6 @@ class ArticlesController < ApplicationController
         @user = User.find(params[:user_id])
     end
     def article_params
-        params.require(:article).permit(:title, :content, :category_elements)
+        params.require(:article).permit(:title, :content, category_elements: [])
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -8,12 +8,14 @@ class Article < ApplicationRecord
     def save_categories
         # category_elements 1,2,3
         # Convertir eso en un arreglo 1,2,3 => [1,2,3]
-        categories_array = category_elements.split(",")
+        # categories_array = category_elements.split(",")
         # Iterar ese arreglo
-        categories_array.each do |category_id|
+        return has_categories.destroy_all if category_elements.nil? || category_elements.empty? 
+        has_categories.where.not(category_id: category_elements).destroy_all 
+        category_elements.each do |category_id|
             # Crear HasCategory HasCategory<article_id:1, category_id: 2>
         #unless HasCategory.where(article:self, category_id: category_id).any?
-        HasCategory.find_or_create_by(article: self, category_id: category_id)
+            HasCategory.find_or_create_by(article: self, category_id: category_id)
         #end
         end
     end
